d37f18bb6679b9274f878cff84212be711496661
Eugene Checkoy <mfalcon@mail.ru>
2021-09-16 17:05:39 +0300

    header index footer

--- alcohol/views.py
+++ alcohol/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     title = 'Moscow Spirits'
-    return render(request, 'index.html', {'title': title})
+    description = 'Крафтовые алкогольные напитки'
+    return render(request, 'index.html', {'title': title, 'description': description})
